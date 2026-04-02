eec0742bd3ce5573be93c04fb25bd5d11553403c
kosmote221 <kosmote221@gmail.com>
2026-04-02 13:40:56 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Made By kosmote_21",
         // Activity type number (0 = Playing).
         type: 0, 
       },
